2d7491d71e18c5472abe415764821c2c3dafba4b
kraddess <kotentuoi1122@gmail.com>
2021-10-14 10:07:49 +0700

    add manage user comp

--- front_end/src/Container/AdminContainer/ManageUser.js
+++ front_end/src/Container/AdminContainer/ManageUser.js
@@ -1,15 +1,107 @@
-import React from 'react'
-import {Row,Col} from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Row, Col } from 'react-bootstrap';
+
+
+
+const UserItem = ({ item }) => {
+    return (
+        <Row className="user-item">
+            <Col xl="1"><div className="object-cover user-item-image"><img src={item.avatar} alt="" /></div></Col>
+            <Col xl="2"><span className="user-item-name">{item.name}</span></Col>
+            <Col xl="3"><span className="user-item-address">{item.address}</span></Col>
+            <Col xl="2"><span className="user-item-phone">{item.phoneNum}</span></Col>
+            <Col xl="2"><span className="user-item-date">{item.dateAdd}</span></Col>
+            <Col xl="2"><button className="user-item-delete">Xóa người dùng</button></Col>
+        </Row>
+    )
+}
+
 const ManageUser = () => {
+    const [userItems, setUserItems] = useState([
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+        {
+            name: "astd",
+            avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
+            dateAdd: "20-09-2020",
+            phoneNum: "321654897",
+            address: "132 asda asd , ádj"
+        },
+    ]);
     return (
         <>
-                <Row>
-                    <h2>Quản lý người dùng</h2>
-                </Row>
-                <Row>
+            <Row>
+                <h2>Quản lý người dùng</h2>
+            </Row>
 
+            <Row className="admin-table-container">
+                <Row className="admin-table-header">
+                    <Col xl="1"><div></div></Col>
+                    <Col xl="2"><h4>Tên người dùng</h4></Col>
+                    <Col xl="3"><h4>Địa chỉ</h4></Col>
+                    <Col xl="2"><h4>Số điện thoại</h4></Col>
+                    <Col xl="2"><h4>Ngày tham gia</h4></Col>
+                    <Col xl="2"></Col>
                 </Row>
-                
+                <Row className="table-user-items">
+                {userItems.map((item) => <UserItem item={item} />)}
+                </Row>
+            </Row>
+
         </>
     )
 }
