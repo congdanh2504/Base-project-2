2ea19207fcb414c50b686ba0bdaff1d434d88975
kraddess <kotentuoi1122@gmail.com>
2021-10-15 15:06:42 +0700

    add blog manage

--- front_end/src/Container/AdminContainer/ManageBlog.js
+++ front_end/src/Container/AdminContainer/ManageBlog.js
@@ -1,15 +1,101 @@
-import React from 'react'
-import {Row,Col} from 'react-bootstrap';
+import React, { useState } from 'react'
+import { Row, Col } from 'react-bootstrap';
+
+
+
+const BlogItem = ({ item }) => {
+    return (
+        <Row className="user-item">
+            <Col xl="1"><div className="object-cover blog-item-image"><img src={item.image} alt="" /></div></Col>
+            <Col xl="1"><span className="user-item-name">{item.id}</span></Col>
+            <Col xl="3"><span className="user-item-address" title={item.title}>{item.title}</span></Col>
+            <Col xl="2"><span className="user-item-phone">{item.author}</span></Col>
+            <Col xl="2"><span className="user-item-date">{item.dateAdd}</span></Col>
+            <Col xl="2"><button className="user-item-delete">Xóa blog</button></Col>
+        </Row>
+    )
+}
+
 const ManageBlog = () => {
+    const [blogItems, setBlogItems] = useState([
+        {
+            id: "B01",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B02",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B03",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B04",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B05",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B06",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B07",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        {
+            id: "B08",
+            title: "blog asjklhaskdjsd ligsda ksjdgh",
+            image: "https://wiki.matbao.net/wp-content/uploads/2019/09/blog-la-gi-2-1200x900.jpg",
+            dateAdd: "20-09-2020",
+            author: "sadjgas",
+        },
+        
+    ]);
     return (
         <>
-                <Row>
-                    <h2>Quản lý blog</h2>
-                </Row>
-                <Row>
+            <Row>
+                <h2>Quản lý Blog</h2>
+            </Row>
 
+            <Row className="admin-table-container">
+                <Row className="admin-table-header">
+                    <Col xl="1"><div></div></Col>
+                    <Col xl="1"><h4>Id</h4></Col>
+                    <Col xl="3"><h4>Tiêu đề</h4></Col>
+                    <Col xl="2"><h4>Tác giả</h4></Col>
+                    <Col xl="2"><h4>Ngày tạo</h4></Col>
+                    <Col xl="2"></Col>
                 </Row>
-                
+                <Row className="table-user-items">
+                {blogItems.map((item) => <BlogItem item={item} />)}
+                </Row>
+            </Row>
+
         </>
     )
 }
--- front_end/src/Container/AdminContainer/ManageUser.js
+++ front_end/src/Container/AdminContainer/ManageUser.js
@@ -8,7 +8,7 @@ const UserItem = ({ item }) => {
         <Row className="user-item">
             <Col xl="1"><div className="object-cover user-item-image"><img src={item.avatar} alt="" /></div></Col>
             <Col xl="2"><span className="user-item-name">{item.name}</span></Col>
-            <Col xl="3"><span className="user-item-address">{item.address}</span></Col>
+            <Col xl="3"><span className="user-item-address" title={item.address}>{item.address}</span></Col>
             <Col xl="2"><span className="user-item-phone">{item.phoneNum}</span></Col>
             <Col xl="2"><span className="user-item-date">{item.dateAdd}</span></Col>
             <Col xl="2"><button className="user-item-delete">Xóa người dùng</button></Col>
@@ -30,7 +30,7 @@ const ManageUser = () => {
             avatar: "https://s3-ap-southeast-1.amazonaws.com/images.spiderum.com/sp-images/e9fef2d083cf11ea8f996dbfbe6e50b1.jpg",
             dateAdd: "20-09-2020",
             phoneNum: "321654897",
-            address: "132 asda asd , ádj"
+            address: "132 asda asd , ádj kjgfj sgdfjhgfjgakjdfgh jajkdfg askdfjg"
         },
         {
             name: "astd",
